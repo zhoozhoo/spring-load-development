b2069e4f24b1eaddd841f6174993a98df54ab853
Zhubin Salehi <zhoozhoo@yahoo.com>
2024-12-28 11:14:32 -0500

    Clean-up and added RefileController

--- loads-service/src/main/java/ca/zhoozhoo/loaddev/load_development/model/Load.java
+++ loads-service/src/main/java/ca/zhoozhoo/loaddev/load_development/model/Load.java
@@ -1,11 +1,9 @@
 package ca.zhoozhoo.loaddev.load_development.model;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-@EntityScan
 @Table(name = "loads")
 public record Load(
 
---
+++ loads-service/src/main/java/ca/zhoozhoo/loaddev/load_development/web/RifleController.java
@@ -0,0 +1,73 @@
+package ca.zhoozhoo.loaddev.load_development.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.zhoozhoo.loaddev.load_development.dao.RifleRepository;
+import ca.zhoozhoo.loaddev.load_development.model.Rifle;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/rifles")
+public class RifleController {
+
+    @Autowired
+    private RifleRepository rifleRepository;
+
+    @GetMapping
+    public Flux<Rifle> getAllRifles() {
+        return rifleRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Rifle>> getRifleById(@PathVariable Long id) {
+        return rifleRepository.findById(id)
+                .map(rifle -> ResponseEntity.ok(rifle))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Rifle> createRifle(@RequestBody Rifle rifle) {
+        return rifleRepository.save(rifle);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<Rifle>> updateRifle(@PathVariable Long id, @RequestBody Rifle rifle) {
+        return rifleRepository.findById(id)
+                .flatMap(existingRifle -> {
+                    Rifle updatedRifle = new Rifle(
+                            existingRifle.id(),
+                            rifle.name(),
+                            rifle.description(),
+                            rifle.caliber(),
+                            rifle.barrelLength(),
+                            rifle.barrelContour(),
+                            rifle.twistRate(),
+                            rifle.freeBore(),
+                            rifle.rifling());
+                    return rifleRepository.save(updatedRifle);
+                })
+                .map(updatedRifle -> ResponseEntity.ok(updatedRifle))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deleteRifle(@PathVariable Long id) {
+        return rifleRepository.findById(id)
+                .flatMap(existingRifle -> rifleRepository.delete(existingRifle)
+                        .then(Mono.just(new ResponseEntity<Void>(HttpStatus.NO_CONTENT))))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
