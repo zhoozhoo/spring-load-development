4330b805e997e782982c99d7e5e4eb8983ca7ae3
Zhubin Salehi <zhoozhoo@yahoo.com>
2025-01-06 15:29:59 -0500

    Added config server

---
+++ spring-loaddev-config-server/src/test/java/ca/zhoozhoo/loaddevelopment/config/ConfigServerApplicationTest.java
@@ -0,0 +1,12 @@
+package ca.zhoozhoo.loaddevelopment.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
