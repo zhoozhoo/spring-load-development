5d07eaaeb2e4e10e22b2568e2b19c7f02a83359c
Zhubin Salehi <zhoozhoo@yahoo.com>
2025-01-05 19:00:13 -0500

    Added docker-compose, eureka-client config

--- spring-loaddev-loads-service/src/main/java/ca/zhoozhoo/loaddev/load_development/LoadDevelopmentApplication.java
+++ spring-loaddev-loads-service/src/main/java/ca/zhoozhoo/loaddev/load_development/LoadDevelopmentApplication.java
@@ -2,10 +2,12 @@ package ca.zhoozhoo.loaddev.load_development;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class LoadDevelopmentApplication {
 
     public static void main(String[] args) {
