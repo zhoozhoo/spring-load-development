f75337400744fda04c05cb3bf6746a39a747abdb
Zhubin Salehi <zhoozhoo@yahoo.com>
2025-02-12 15:59:59 -0500

    Added missing annotation

--- spring-loaddev-api-gateway/src/main/java/ca/zhoozhoo/loaddev/api/config/SecurityConfiguration.java
+++ spring-loaddev-api-gateway/src/main/java/ca/zhoozhoo/loaddev/api/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package ca.zhoozhoo.loaddev.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
+    @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchanges -> exchanges
