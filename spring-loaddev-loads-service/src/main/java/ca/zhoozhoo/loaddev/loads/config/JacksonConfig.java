69d8260bca8b4b8d7787a28e8375cda717b3b96d
Zhubin Salehi <zhoozhoo@yahoo.com>
2025-11-06 16:50:13 -0500

    fix: Remove unused Log4j2 annotation from JacksonConfig

--- spring-loaddev-loads-service/src/main/java/ca/zhoozhoo/loaddev/loads/config/JacksonConfig.java
+++ spring-loaddev-loads-service/src/main/java/ca/zhoozhoo/loaddev/loads/config/JacksonConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.Module;
 
 import ca.zhoozhoo.loaddev.loads.converter.QuantityModule;
-import lombok.extern.log4j.Log4j2;
 
 @Configuration
-@Log4j2
 public class JacksonConfig {
 
     @Bean
